4930826374c44e271edba1c67aca52d3b9644f61
Tonow <nwck.thomas@gmail.com>
2018-03-12 00:17:42 +0100

    output csv volume

--- MQTT/App/serveur_lire_csv_7.py
+++ MQTT/App/serveur_lire_csv_7.py
@@ -5,45 +5,64 @@ import setting
 from datetime import datetime
 
 cwd = os.getcwd()
+entete_csv = ("Sortie","Date","Delta temps","Volume")
+list_retour = []
 date_precedante = False
 debit_precedant = 0
 volume_precedant = 0
 somme_volume = 0
 
-for file in os.listdir('.'):
-    if file.endswith(".csv"):
-        print(file)
+def propose_fichier_csv():
+    for file in os.listdir('.'):
+        if file.endswith(".csv"):
+            print(file)
 
-ficher_a_traiter = input("\nNom du fichier a traiter : ")
-with open(ficher_a_traiter, newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        topic = row[0]
-        date = row[1]
-        debit = row[2]
+    ficher_a_traiter = input("\nNom du fichier a traiter : ")
+    return ficher_a_traiter
 
-        # Evite la premiere ligne ou sont ecrite les nom des colonnes
-        if len(date) > 4:
-            date_format = datetime.strptime(date, "%Y/%m/%d-%H:%M:%S")
+def calcule_volumes_consomation_eau(ficher_a_traiter, date_precedante = False, debit_precedant = 0, volume_precedant = 0, somme_volume = 0):
+    with open(ficher_a_traiter, newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            topic = row[0]
+            date = row[1]
+            debit = row[2]
 
-            if debit == 0 or not date_precedante:
-                date_precedante = date_format
-                delta_temps = 0
-                volume = 0
-            else:
-                delta_temps = (date_format - date_precedante).total_seconds()
-                volume = delta_temps * float(debit_precedant)
-                somme_volume = somme_volume + volume
+            # Evite la premiere ligne ou sont ecrite les nom des colonnes
+            if len(date) > 4:
+                date_format = datetime.strptime(date, "%Y/%m/%d-%H:%M:%S")
 
-            volume_precedant = volume
-            debit_precedant = debit
+                if debit == 0 or not date_precedante:
+                    date_precedante = date_format
+                    delta_temps = 0
+                    volume = 0
+                else:
+                    delta_temps = (date_format - date_precedante).total_seconds()
+                    volume = delta_temps * float(debit_precedant)
+                    somme_volume = somme_volume + volume
 
-            if setting.debug:
-                print(f"{debit} : {delta_temps} : {volume} : {somme_volume}")
+                volume_precedant = volume
+                debit_precedant = debit
 
-        if debit == "0":
-            if setting.debug:
-                print(f"{topic} : {date_format} : {debit} : {delta_temps} : {volume} : {somme_volume}")
-            date_precedante = False
-            volume_precedant = 0
-            somme_volume = 0
+                if setting.debug:
+                    print(f"{debit} : {delta_temps} : {volume} : {somme_volume}")
+
+            if debit == "0":
+                list_retour.append((topic,date_format,delta_temps,somme_volume))
+                if setting.debug:
+                    print(f"{topic} : {date_format} : {debit} : {delta_temps} : {volume} : {somme_volume}")
+                date_precedante = False
+                volume_precedant = 0
+                somme_volume = 0
+    csv_ouput(list_retour, ficher_a_traiter)
+
+def csv_ouput(list_retour, nom_fichier):
+    with open('Volume' + nom_fichier[5:] + '.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(entete_csv)
+        for item in list_retour:
+            ligne = (item[0], item[1], item[2], item[3])
+            writer.writerow(ligne)
+
+ficher_a_traiter = propose_fichier_csv()
+calcule_volumes_consomation_eau(ficher_a_traiter)
